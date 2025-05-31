59f3ac05b8604f84b1fb8c5df97e67b2a3fc07a8
thomasjbell <thomas.bell.fusion@gmail.com>
2025-05-31 15:21:31 +0100

    most recent -> bloody broken

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,14 @@
-import { path } from "path";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = [...compat.extends("next/core-web-vitals")];
 
 export default eslintConfig;
-
