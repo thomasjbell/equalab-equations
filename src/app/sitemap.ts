511fc8ad11ab9a4bd17561390f37a1631ac854fb
thomasjbell <thomas.bell.fusion@gmail.com>
2025-07-09 20:33:33 +0100

    sitemap adjustment

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -21,7 +21,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 1,
     },
     {
-      url: `${baseUrl}/favorites`,
+      url: `${baseUrl}/favourites`,
       lastModified: new Date(),
       changeFrequency: 'weekly' as const,
       priority: 0.8,
